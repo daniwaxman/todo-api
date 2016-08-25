e7472e62e202b07411d76316509b77dac3249316
daniwaxman <daniwaxman@gmail.com>
2016-08-25 14:50:23 +0300

    refactoring and add underscore library

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app  = express();
 var PORT = process.env.PORT || '3000';
 var todos = [];
@@ -20,15 +22,10 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoID = parseInt(req.params.id, 10);
-    var matchedtodo ;
+    var matchedTodo = _.findWhere(todos, {id: todoID});
 
-    todos.forEach(function (todo){
-        if(todoID === todo.id){
-            matchedtodo = todo
-        }
-    });
-    if (matchedtodo) {
-        res.json(matchedtodo);
+    if (matchedTodo) {
+        res.json(matchedTodo);
     } else {
         res.status(404).send();
     }
@@ -36,9 +33,14 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos
 app.post ('/todos', function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
 
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
     body.id = todoNextId++;
+    body.description = body.description.trim();
+
     todos.push(body);
     res.json(body);
 });
