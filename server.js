3c59e92e5e85f8802582b04b74d6ae107ad9439e
daniwaxman <daniwaxman@gmail.com>
2016-08-28 18:08:20 +0300

    added database POST

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js')
 
 var app  = express();
 var PORT = process.env.PORT || '3000';
@@ -52,15 +53,28 @@ app.get('/todos/:id', function (req, res) {
 app.post ('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-        return res.status(400).send();
-    }
-    body.id = todoNextId++;
-    body.description = body.description.trim();
 
-    todos.push(body);
-    res.json(body);
-});
+    //db.todo.create
+    //if good respond with 200 and object
+    //if fails pass e res.json and status 400
+
+    db.todo.create(body).then (function(todo) {
+        res.json(todo.toJSON());
+    }), function(e) {
+        res.status(400).json(e);
+    }
+    });
+
+
+    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+    //     return res.status(400).send();
+    // }
+    // body.id = todoNextId++;
+    // body.description = body.description.trim();
+    //
+    // todos.push(body);
+    // res.json(body);
+
 
 
 //DELETE /todos/:id
@@ -105,8 +119,9 @@ app.put ('/todos/:id', function (req, res) {
     res.json(matchedTodo);
 
 });
-
-app.listen(PORT, function (){
-    console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function (){
+    app.listen(PORT, function (){
+        console.log('Express listening on port ' + PORT + '!');
+    });
 });
 
