5608856f9ab55fe8313cfd67367f4be72abef19d
daniwaxman <daniwaxman@gmail.com>
2016-08-28 21:43:15 +0300

    convert GET todos:is to use db

--- server.js
+++ server.js
@@ -40,23 +40,22 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoID = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoID});
 
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
+    db.todo.findById(todoID).then(function(todo) {
+        if (!!todo) {
+            res.json(todo.toJSON());
+        } else {
+            return res.status(404).send();
+        }
+    }, function(e) {
+        return res.status(500).send();
+    });
+
 });
 
 //POST /todos
 app.post ('/todos', function(req, res){
     var body = _.pick(req.body, 'description', 'completed');
-
-
-    //db.todo.create
-    //if good respond with 200 and object
-    //if fails pass e res.json and status 400
 
     db.todo.create(body).then (function(todo) {
         res.json(todo.toJSON());
@@ -64,18 +63,6 @@ app.post ('/todos', function(req, res){
         res.status(400).json(e);
     }
     });
-
-
-    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-    //     return res.status(400).send();
-    // }
-    // body.id = todoNextId++;
-    // body.description = body.description.trim();
-    //
-    // todos.push(body);
-    // res.json(body);
-
-
 
 //DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
